31bf867f491ac71046b46d2003193d5a9d3e26a2
natyiano <jiaji5@live.com>
2022-04-24 22:54:38 +0800

    final

--- bigger.h
+++ bigger.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <cstring>
 
- template <typename T>
- inline T bigger(T x, T y)
+ template <class T,class T2>
+ inline T bigger(T x, T2 y)
 {
     return x > y ? x: y;
+}
+inline std::string bigger(const std::string& x, const std::string& y)
+{
+    return x.length() > y.length() ? x: y;
+}
+
+inline const char * bigger(const char * x, const char * y)
+{
+    return strlen(x) > strlen(y) ? x: y;
 }
